cc7858ddc3d08cc34d5278c0469b4ccb3def2f56
Markus Fischer <fischer@mideasdesign.de>
2025-04-23 17:14:49 +0200

    animate chicken

--- assets/js/models/chicken.class.js
+++ assets/js/models/chicken.class.js
@@ -2,9 +2,25 @@ class Chicken extends MovableObject{
 
     width = 50;
     height = 70;
+    images_walking = [
+        'assets/images/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+        'assets/images/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+        'assets/images/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
+    ];
+    currentImage = 0;
     constructor(){
         super().loadImage('assets/images/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.x = 200 + Math.random() * 500;
         this.y = 430 - this.height;
+        this.loadImages(this.images_walking);
+        this.animate();
+    };
+    animate(){
+        setInterval(() => {
+            let i = this.currentImage % this.images_walking.length;
+            let path = this.images_walking[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 200);
     }
 }
