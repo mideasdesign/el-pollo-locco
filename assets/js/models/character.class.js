d9b4807c2e8dbde0e279d647165c4259c06933f9
Markus Fischer <fischer@mideasdesign.de>
2025-04-23 11:28:39 +0200

    animate pepe

--- assets/js/models/character.class.js
+++ assets/js/models/character.class.js
@@ -3,18 +3,30 @@ class Character extends MovableObject{
     y = 130;
     width = 150;
     height = 300;
-
+    images_walking = [
+        'assets/images/2_character_pepe/2_walk/W-21.png',
+        'assets/images/2_character_pepe/2_walk/W-22.png',
+        'assets/images/2_character_pepe/2_walk/W-23.png',
+        'assets/images/2_character_pepe/2_walk/W-24.png',
+        'assets/images/2_character_pepe/2_walk/W-25.png',
+        'assets/images/2_character_pepe/2_walk/W-26.png'
+    ];
+    currentImage = 0;
     constructor(){
         super().loadImage('assets/images/2_character_pepe/2_walk/W-21.png');
-        this.loadImages([
+        this.loadImages(this.images_walking);
+        this.animate();
+    }
 
-            'assets/images/2_character_pepe/2_walk/W-21.png',
-            'assets/images/2_character_pepe/2_walk/W-22.png',
-            'assets/images/2_character_pepe/2_walk/W-23.png',
-            'assets/images/2_character_pepe/2_walk/W-24.png',
-            'assets/images/2_character_pepe/2_walk/W-25.png',
-            'assets/images/2_character_pepe/2_walk/W-26.png'
-        ]);
+    // let i = 0 % 6; => 0, Rest 0  -  let i = 1 % 6; 0 Rest 1  -  let i = 2 % 6; 0 Rest 2
+    // i = 0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0
+    animate(){
+        setInterval(() => {
+            let i = this.currentImage % this.images_walking.length;
+            let path = this.images_walking[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 200);
     }
     jump(){
 
--- assets/js/models/movable-objects.class.js
+++ assets/js/models/movable-objects.class.js
@@ -7,29 +7,21 @@ class MovableObject {
     imageCache = {};
  
 
-loadImage(path){
-    this.img = new Image();
-    this.img.src = path;
-};
-/**
- * 
- * @param { Array
- * } arr - ['assets/images/img1.png, assets/images/img1.png, ....']
- */
-loadImages(arr){
-    arr.forEach((path) => {
-        let img = new Image();
-        img.src = path;
-        this.imageCache[path] = path;
-    });
-}
-
-    moveRight(){
- 
-        
-    }
-    moveLeft(){
-
+    loadImage(path){
+        this.img = new Image();
+        this.img.src = path;
+    };
+    /**
+     * 
+     * @param { Array
+     * } arr - ['assets/images/img1.png, assets/images/img1.png, ....']
+     */
+    loadImages(arr){
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
     }
 
 }
