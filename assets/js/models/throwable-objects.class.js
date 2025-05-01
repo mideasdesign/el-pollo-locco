8652ea18a423b4f734207746f51368e476cce277
Markus Fischer <fischer@mideasdesign.de>
2025-05-01 14:33:10 +0200

    recreate the the rotating bottle

--- assets/js/models/throwable-objects.class.js
+++ assets/js/models/throwable-objects.class.js
@@ -1,27 +1,51 @@
 class ThrowableObject extends MovableObject {
-/*   width = 1900;
-  height = 300;
-  speed = 0.15; */
+
+  images_rotating_bottle = [
+    'assets/images/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+    'assets/images/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+    'assets/images/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+    'assets/images/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png'
+];
+
+images_splash = [
+  'assets/images/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
+  'assets/images/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
+  'assets/images/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
+  'assets/images/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
+  'assets/images/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
+  'assets/images/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png'
+
+];
 
   constructor(x, y) {
     super().loadImage('assets/images/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png');
     this.x = 100;
     this.y = 100;
+    this.height = 70;
+    this.width = 50;
+    this.loadImages(this.images_rotating_bottle);
+    this.animate();
     this.throw(100, 300);
     
 
   };
+
+  animate(){
+    setInterval(() => {
+      this.x += 40; 
+        this.playAnimation(this.images_rotating_bottle);
+    }, 1000 / 12);
+}
 
   throw(x, y){
     this.x = x;
     this.y = y;
     this.height = 70;
     this.width= 90;
-    this.speedY = 20;
+    this.speedY = 22;
+    this.speed = 30;
     this.applyGravity();
-    setInterval(() => {
-      this.x += 20;
-    }, 25)
+    this.animate();
   };
 
 }
